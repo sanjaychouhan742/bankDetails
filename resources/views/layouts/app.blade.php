61efc1cf0cbf0082ef6d7d4e6d5a9276eb5bd97e
Sanjay <sanjaychouhan742@gmail.com>
2022-07-15 21:12:45 +0530

    delete

--- resources/views/layouts/app.blade.php
+++
@@ -1,7 +0,0 @@
-@include('layouts/header')
-@include('layouts/side')
-@include('sweetalert::alert')
-<div class="content-wrapper">
-	@yield('content')
-</div>
-@include('layouts/footer')
--- resources/views/welcome/Welcome.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-@section('content')
-<div class="content">
-	<div class="container-wrapper">
-		<h1>Hello Admin</h1>
-	</div>
-</div>
-@include('sweet::alert')
-@endsection
